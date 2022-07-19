60eb4517b186e3b3faf4ff9dd553f37e72467baa
Livelyoneweek <wlvvwhs@naver.com>
2022-07-19 21:59:07 +0900

    0719 webrtc 1-1(front) base

---
+++ src/test/java/web/webrtc/WebrtcApplicationTests.java
@@ -0,0 +1,13 @@
+package web.webrtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrtcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
